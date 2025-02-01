45bb79a28ed567d33340f27aa78a3eab0ad3cbc0
FaBarbaNera <fabiogiorgio84@yahoo.it>
2025-02-01 17:17:41 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Script per il menu hamburger
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const navLinks = document.getElementById('nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
